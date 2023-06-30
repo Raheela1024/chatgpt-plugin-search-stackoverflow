69cceaf4c5aeec17844d882d5c0111a0bf694e0a
raheela <raheela@ebi.ac.uk>
2023-06-30 15:12:28 +0100

    api called to get joke.

--- stackoverflow-search/src/python/index.py
+++ stackoverflow-search/src/python/index.py
@@ -1,5 +1,8 @@
 # connect to openai api and get the response
 # from the api
+import requests
+
+
 def get_response(prompt):
     response = openai.Completion.create(
         engine="davinci",
@@ -13,10 +16,12 @@ def get_response(prompt):
     )
     return response
 
+
 # write a python program to get the response from the api and print it
 def print_response(prompt):
     response = get_response(prompt)
     print(response.choices[0].text)
+
 
 # write a python program to get the response from the api and return it
 
@@ -33,11 +38,25 @@ def get_response(prompt):
     )
     return response.choices[0].text
 
+
 def setupAPI():
     import openai
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
+def get_joke():
+    return requests.get(
+        "https://icanhazdadjoke.com/", headers={'Accept': 'text/plain'})
+
+
 def main():
     setupAPI()
     prompt = "Human: Hello, who are you? I'm a Robot\nAI:"
+    response = get_joke()
+    if response.status_code == 200:
+        print(response.text)
     print_response(prompt)
+
+
+if __name__ == "__main__":
+    main()
