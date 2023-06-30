71307a881a1d56837504fab0e59ac6efb8165374
raheela <raheela@ebi.ac.uk>
2023-06-30 15:24:49 +0100

    conflicts resolved.

--- stackoverflow-search/src/python/index.py
+++ stackoverflow-search/src/python/index.py
@@ -1,47 +1,28 @@
 # connect to openai api and get the response
 # from the api
 import requests
+from dotenv import load_dotenv
+import os
+import openai
+
+# You need to have .env file in the same directory as this file with you
+# OpenAI API key like this: OPENAI_API_KEY=your_api_key
+load_dotenv()
+
+api_key = os.getenv('OPENAI_API_KEY')
+
+openai.api_key = api_key
 
 
-def get_response(prompt):
+def chat_with_gpt(message):
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", " Human:", " AI:"]
+        engine="text-ada-001",
+        prompt=message,
+        temperature=0.5,
+        max_tokens=100
     )
-    return response
 
-
-# write a python program to get the response from the api and print it
-def print_response(prompt):
-    response = get_response(prompt)
-    print(response.choices[0].text)
-
-
-# write a python program to get the response from the api and return it
-
-def get_response(prompt):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", " Human:", " AI:"]
-    )
-    return response.choices[0].text
-
-
-def setup_api():
-    import openai
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    return response.choices[0].text.strip()
 
 
 def get_joke():
@@ -49,14 +30,10 @@ def get_joke():
         "https://icanhazdadjoke.com/", headers={'Accept': 'text/plain'})
 
 
-def main():
-    setup_api()
-    prompt = "Human: Hello, who are you? I'm a Robot\nAI:"
-    print_response(prompt)
-    response = get_joke()
-    if response.status_code == 200:
-        print(response.text)
+user_message = input("User: ")
+gpt_response = chat_with_gpt(user_message)
+print("GPT: ", gpt_response)
+response = get_joke()
+if response.status_code == 200:
+    print(response.text)
 
-
-if __name__ == "__main__":
-    main()
